13deb9ae26984aa72b8a63abf620371e4b997f4e
cherki hamza <cherki0hamza@gmail.com>
2020-03-15 21:48:09 +0100

    cherki hamza portfolio

---
+++ app.js
@@ -0,0 +1,9 @@
+
+console.log("%c hello from cherki hamza develope web full stack , and lahe chafike :) !!." , `
+    color:black;
+    border-radius:6px ;
+    font-weight: bold;
+    background-image: linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%);
+    font-size:20px;
+    text-shadow: salmon 2px 2px 2px;
+`);
